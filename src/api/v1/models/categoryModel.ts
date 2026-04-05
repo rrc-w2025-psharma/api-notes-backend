fa709bac99ed50db52e6f6c5d496d723cd0d0041
Princepreet Sharma <psharma40@rrc.ca>
2026-04-04 21:16:30 -0500

    Updated the model files.

--- src/api/v1/models/categoryModel.ts
+++ src/api/v1/models/categoryModel.ts
@@ -1,5 +1,12 @@
 export interface Category {
     id: string;
-    userId: string;
     name: string;
+}
+
+export interface CreateCategoryInput {
+    name: string;
+}
+
+export interface UpdateCategoryInput {
+    name?: string;
 }
