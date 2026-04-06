fadae31e78382cb6a7b23172110d101427053ebe
Princepreet Sharma <psharma40@rrc.ca>
2026-04-06 14:07:44 -0500

    feat: Add user and admin in app.

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -3,6 +3,8 @@ import { healthCheckRoute } from "./healthRoute";
 import noteRoutes from "./noteRoutes";
 import categoryRoutes from "./categoryRoutes";
 import tagRoutes from "./tagRoutes";
+import userRoutes from "./userRoutes";
+import adminRoutes from "./adminRoutes";
 
 const router = Router();
 
@@ -10,5 +12,8 @@ router.use(healthCheckRoute);
 router.use(categoryRoutes);
 router.use(noteRoutes);
 router.use(tagRoutes);
+router.use(tagRoutes);
+router.use(userRoutes);
+router.use(adminRoutes);
 
 export default router;
